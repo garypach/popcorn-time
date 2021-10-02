8c25758b528807ab0f707508256d8cb53982ac83
Gary <58466698+garypach@users.noreply.github.com>
2021-10-02 15:20:10 -0700

    added video prop to featured.js

--- components/featured.js
+++ components/featured.js
@@ -7,7 +7,7 @@ const FeaturedVideo = (props) =>
            className="video"
            width="100%" 
            height="100%" 
-           src="https://www.youtube.com/embed/8hjB6UJ2kMU?autoplay=1&loop=1&start=1&mute=1" title="Jurrassic Park" 
+           src= {props.video} title={props.title} 
            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen></iframe>
        </div>
