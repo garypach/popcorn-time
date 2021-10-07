be471b0f33bcd9dfb78f3ed0a10d60094e9f84c6
Gary <58466698+garypach@users.noreply.github.com>
2021-10-07 13:08:50 -0700

    featuremedia now dynamic for each page

--- pages/[mediatype]/genres/[genre_id].js
+++ pages/[mediatype]/genres/[genre_id].js
@@ -10,35 +10,40 @@ import LazyLoad from 'react-lazyload'
 import Skeleton from '../../../components/skeleton'
 import GenreNav from '../../../components/genrenav'
 import axios from 'axios'
+import { shuffle } from '../../../components/util/utilityfunctions'
 export default function Index(props){
   const router = useRouter()
   const { id } = router.query
 
-  // const [error, setError] = useState(null);
-  // const [isLoaded, setIsLoaded] = useState(false);
-  // const [media,setMedia] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [media,setMedia] = useState([]);
 
-  // // Note: the empty deps array [] means
-  // // this useEffect will run once
-  // // similar to componentDidMount()
-  // useEffect(() => {
-  //   fetch(`https://api.themoviedb.org/3/genre/${props.query.mediatype}/list?api_key=${api_key}`)
-  //     .then(res => res.json())
-  //     .then(
-  //       (result) => {
-  //         setIsLoaded(true);
-  //         setMedia(result.genres);
-  //         console.log(result)
-  //       },
-  //       // Note: it's important to handle errors here
-  //       // instead of a catch() block so that we don't swallow
-  //       // exceptions from actual bugs in components.
-  //       (error) => {
-  //         setIsLoaded(true);
-  //         setError(error);
-  //       }
-  //     )
-  // }, [])
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/${props.query.mediatype}/${props.genreData.id}/videos?api_key=${api_key}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setMedia(result.results);
+          console.log(result)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [props.query.mediatype,props.genreData.id])
+ const onekey = media
+            .map(keys=>keys.key);
+  shuffle(onekey);
   
 
   const showRandomMedia = () => {
@@ -51,8 +56,9 @@ export default function Index(props){
   
   return (
     <MainLayout>
-       <FeaturedVideo mediaURL="https://www.youtube.com/embed/8hjB6UJ2kMU?autoplay=1&loop=1&start=1&mute=1" title="Jurrassic Park"/>
+       <FeaturedVideo mediaURL={`https://www.youtube.com/embed/${onekey[0]}?autoplay=1&loop=1&start=1&mute=1&playlist=${onekey[0]}`} title={props.genreData.name ? props.genreData.name : props.genreData.title}/>
        <GenreNav mediaType={props.query.mediatype} genreData={props.mediaData}/>
+       <p className="genreHeading">{props.genreData.name ? "TV Shows" : "Movies"}</p>
        {showRandomMedia()}
     </MainLayout>
   )
@@ -60,12 +66,16 @@ export default function Index(props){
 
 export async function getServerSideProps(context) {
   let mediaData;
+  let genreData;
   try{
     mediaData = await axios.get(`https://api.themoviedb.org/3/genre/${context.query.mediatype}/list?api_key=${api_key}`);
     console.log(mediaData)
+    genreData = await axios.get(`https://api.themoviedb.org/3/discover/${context.query.mediatype}?with_genres=${context.query.genre_id}&language=en-US&sort_by=popularity.desc&include_video=true&primary_release_year=2021&api_key=${api_key}`);
+    console.log(genreData)
+
   }catch(error){
     console.log(error);
   }
   // Pass data to the page via props
-  return { props: {query: context.query, mediaData: mediaData.data.genres,} }
+  return { props: {query: context.query, mediaData: mediaData.data.genres, genreData: shuffle(genreData.data.results)[0]} }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,60 @@
-import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import api_key from '../api'
+import { shuffle } from '../components/util/utilityfunctions'
+import axios from 'axios'
 import Home from '../components/home'
+import FeaturedVideo from '../components/featured'
+import MainLayout from '../components/mainlayout'
 
+export default function Index(props) {
 
-export default function index() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [media,setMedia] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${props.videoData.id}/videos?api_key=${api_key}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setMedia(result.results);
+          console.log(result)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [props.videoData.id])
+ const onekey = media
+            .map(keys=>keys.key);
+  shuffle(onekey);
+
   return (
+    <MainLayout>
+    <FeaturedVideo mediaURL={`https://www.youtube.com/embed/${onekey[0]}?autoplay=1&loop=1&start=1&mute=1&playlist=${onekey[0]}`} title={props.videoData.title}/>
     <Home/>
+    </MainLayout>
   )
+}
+
+export async function getServerSideProps() {
+  let videoData;
+  try{
+    videoData = await axios.get(`https://api.themoviedb.org/3/discover/movie?&language=en-US&sort_by=popularity.desc&include_video=true&api_key=${api_key}`);
+    console.log(videoData)
+
+  }catch(error){
+    console.log('error');
+
+    console.log(error);
+  }
+  // Pass data to the page via props
+  return { props: {videoData: shuffle(videoData.data.results)[0]} }
 }
