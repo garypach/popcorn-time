464377b13442570b9d9e9e2dc4621748bf7c6e4f
Gary <58466698+garypach@users.noreply.github.com>
2022-02-17 19:40:33 -0800

    remove hasmounted from auth

--- components/AuthCheck.js
+++ components/AuthCheck.js
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import ls from "local-storage";
-import { useMounted } from "./util/useMounted";
 
 
 const AuthCheck = (component) => {
   const router = useRouter();
-  const {hasMounted} = useMounted();
   let activeUID = ls('activeUID');
   let users = ls('users') !== null ? ls('users') : [];
 
